c2179294a1ea580223fc0bd79da807568b9210e4
khoi <leminhkhoi.cuchi@gmail.com>
2025-05-17 12:26:42 +0700

    Tạm commit thay đổi trước khi chuyển sang nhánh LMKhoi

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1 style={{ textAlign: 'center', marginTop: '50px' }}>
+        Welcome to Student Manager!
+      </h1>
+    </main>
+  );
+}
